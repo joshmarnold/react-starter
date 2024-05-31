ae1f4964cfece0deb07d750d4cc6f00b9b36b657
Joshua Arnold <jarnld2@gmail.com>
2024-05-30 20:21:33 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { Container } from "@mui/material";
 import { NavBar } from "./components/NavBar";
-import { LandingPage } from "./pages/LandingPage";
 import { HomePage } from "./pages/HomePage";
 
 export const App: React.FC = () => {
@@ -10,8 +9,7 @@ export const App: React.FC = () => {
     <Container>
       <NavBar />
       <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/home" element={<HomePage />} />
+        <Route path="/" element={<HomePage />} />
       </Routes>
     </Container>
   );
