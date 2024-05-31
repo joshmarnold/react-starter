7e678ee812f5daecdb3caf66e12e52ecdb81e596
Joshua Arnold <jarnld2@gmail.com>
2024-05-30 20:43:51 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,5 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ["**/*.mp3"],
-  resolve: {
-    alias: {
-      "components": path.resolve(__dirname, "src/components"),
-      "hooks": path.resolve(__dirname, "src/hooks"),
-      "store": path.resolve(__dirname, "src/store"),
-      supabase: path.resolve(__dirname, "src/supabaseClient.ts"),
-      src: path.resolve(__dirname, "src"), 
-    },
-  },
+  assetsInclude: ["**/*.mp3"]
 });
